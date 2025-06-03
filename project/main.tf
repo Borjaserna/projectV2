763f747ce2d631e83bc3a88b420395c4055160b3
Borjaserna <fdb.serna@gmail.com>
2025-06-03 17:45:19 +0200

    lb

--- project/main.tf
+++ project/main.tf
@@ -17,8 +17,8 @@ module "compute" {
   source          = "./modules/compute"
   vm_name         = "vm-security"
   location        = var.location
-  resource_group = azurerm_resource_group.main.name
-  subnet_id = module.network.subnet_id
+  resource_group  = azurerm_resource_group.main.name
+  subnet_id       = module.network.subnet_id
   public_ip_id    = module.network.vm_public_ip
   vm_size         = var.vm_size
   admin_user      = var.admin_user
@@ -30,8 +30,8 @@ module "scale_set_lb" {
   source         = "./modules/scale_set_lb"
   location       = var.location
   resource_group = azurerm_resource_group.main.name
-  subnet_id = module.network.subnet_id
-  public_ip_id   = module.network.vm_public_ip
+  subnet_id      = module.network.subnet_id
+  public_ip_id   = module.network.lb_public_ip
   vm_size        = var.vm_size
   admin_user     = var.admin_user
   admin_password = var.admin_password
